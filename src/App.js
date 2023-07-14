f41a5c216cca68c1aaac1295d0570fd0010efcf7
deepak14ri <49471265+deepak14ri@users.noreply.github.com>
2023-07-14 19:55:47 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -47,42 +47,40 @@ export default function App() {
 
 // ============Merge 2 duplicate array and sort===========
 
-const sortDup = (arr) => {
-  const removeDup = (arr) => {
-    let uniq_arr = [];
-    for (let i = 0; i < arr.length; i++) {
-      let isUniq = false;
-      let element = arr[i];
-      // debugger
-      for (let j = 0; j < uniq_arr.length; j++) {
-        if (element === uniq_arr[j]) {
-          isUniq = true;
-          break;
-        }
-      }
-      if (!isUniq) {
-        uniq_arr.push(element);
+const removeDup = (arr) => {
+  let uniq_arr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let isUniq = false;
+    let element = arr[i];
+    // debugger
+    for (let j = 0; j < uniq_arr.length; j++) {
+      if (element === uniq_arr[j]) {
+        isUniq = true;
+        break;
       }
     }
-    return uniq_arr;
-  };
-  var newArr = removeDup(arr);
-  const sorting = (newArr) => {
-    for (let i = 0; i < newArr.length; i++) {
-      for (let j = 0; newArr.length - i; j++) {
-        if (newArr[j] > newArr[j + 1]) {
-          // [newArr[j], newArr[j+1]] = [newArr[j+1], newArr[j]];
-          let temp = newArr[j];
-          newArr[j] = newArr[j + 1];
-          newArr[j + 1] = temp;
-        }
+    if (!isUniq) {
+      uniq_arr.push(element);
+    }
+  }
+  return uniq_arr;
+};
+
+const sortDup = (newArr) => {
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; newArr.length - i; j++) {
+      if (newArr[j] > newArr[j + 1]) {
+        // [newArr[j], newArr[j + 1]] = [newArr[j + 1], newArr[j]];
+        let temp = newArr[j];
+        newArr[j] = newArr[j + 1];
+        newArr[j + 1] = temp;
       }
     }
-    return sorting(newArr);
-  };
+  }
   return newArr;
 };
 var Arr_A = ['Ramesh', 'Suresh', 'Deepak', 'Suresh', 'Gopi'];
 var Arr_B = ['Gopi', 'vendeesh', 'Deepak', 'Vedneesh'];
 var arr = [...Arr_A, ...Arr_B];
-console.log(sortDup(arr));
+var newArr = removeDup(arr);
+console.log(sortDup(newArr));
